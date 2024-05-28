1408479498b2b35d5cd9213ac6b3fb98a8bedef6
treeplanter <102788310+r00tPl4nt3r@users.noreply.github.com>
2024-05-28 21:33:30 +0200

    so far not so good.

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ docker image build ./ft-sensor/ -t ft/ft-sensor
 docker image build ./ft-ui/ -t ft/ft-ui 
 cd deployments/FT24V/docker
 docker image build ./ft-ui/ -t ft/ft-ui-opcua
+docker image build ./opcua-plc -t plc/opcua
