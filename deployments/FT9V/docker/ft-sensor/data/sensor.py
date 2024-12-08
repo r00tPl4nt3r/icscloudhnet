4fd00467ed52a6c5860704e7ce2c84ed7fde88fe
treeplanter <102788310+r00tPl4nt3r@users.noreply.github.com>
2024-12-08 20:25:51 +0100

    remove old file.

--- deployments/FT9V/docker/ft-sensor/data/sensor.py
+++ deployments/FT9V/docker/ft-sensor/data/sensor.py
@@ -2,45 +2,42 @@
 from datetime import datetime, timezone
 import paho.mqtt.client as mqtt
 import json
-from IPython import display
-import base64
 import time
 import random
 
 
-port = 1883
+mqtt_port = 1883
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.connect("ft-broker", port=port)
-
-
-data = {}
-with open('./data/img/21.jpg', mode='rb') as file:
-    img = file.read()
-
-icamera={ 
-    "data" : "data:image/jpeg;base64,"+base64.encodebytes(img).decode('utf-8'),
-    "ts" : datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-    }
-
-client.connect("ft-broker", port=port)
-topic = "i/cam"
-message= json.dumps(icamera)
-client.publish(topic, message)
 
 while True:
-    ibme680={"aq" : round(random.gauss(15, 1),1) ,
-        "gr" : 836963.0,
-        "h" : round(random.gauss(23, 1),0),
-        "iaq" : round(random.gauss(133, 3),0),
-        "p" : round(random.gauss(980, 10),1),
-        "rh" : round(random.gauss(22, 1),0),
-        "rt" : round(random.gauss(23, 0.5),2),
-        "t" : round(random.gauss(21, 0.5),0),
-        "ts" : datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")}
-    client.connect("ft-broker", port=port)
-    topic = "i/bme680"
-    message = json.dumps(ibme680)
-    client.publish(topic, message)
+    ibme680 = {
+        "aq": 0,
+        "gr": 836963.0,
+        "h": 0,
+        "iaq": 0,
+        "p": 0,
+        "rh": 0,
+        "rt": 0,
+        "t": 0,
+        "ts": ""
+    }
 
-    time.sleep(60)
+while True:
+    try:
+        ibme680["aq"] = round(random.gauss(15, 1), 1)
+        ibme680["h"] = round(random.gauss(23, 1), 0)
+        ibme680["iaq"] = round(random.gauss(133, 3), 0)
+        ibme680["p"] = round(random.gauss(980, 10), 1)
+        ibme680["rh"] = round(random.gauss(22, 1), 0)
+        ibme680["rt"] = round(random.gauss(23, 0.5), 2)
+        ibme680["t"] = round(random.gauss(21, 0.5), 0)
+        ibme680["ts"] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
+        mqtt_topic = "i/bme680"
+        message = json.dumps(ibme680)
+        client.publish(mqtt_topic, message)
+
+        time.sleep(60)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        time.sleep(5)
