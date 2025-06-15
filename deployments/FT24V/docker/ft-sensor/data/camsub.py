1da1d55604ddb7996f812b1d0c151b4a1e91d25e
r00tpl4nt3r <urregdie@b-tu.de>
2025-06-15 15:55:30 +0200

    wowwowo

--- deployments/FT24V/docker/ft-sensor/data/camsub.py
+++ deployments/FT24V/docker/ft-sensor/data/camsub.py
@@ -26,7 +26,7 @@ def on_connect(client, userdata, flags, reason_code, properties):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     global x, y
-    logging.info(f"Received from sbscription topic: {msg.topic} message: {msg.payload}")
+    logger.info(f"Received from sbscription topic: {msg.topic} message: {msg.payload}")
 
     jsonmsg = json.loads(msg.payload.decode())
 
@@ -53,7 +53,7 @@ def on_message(client, userdata, msg):
     topic = "i/cam"
     message = json.dumps(icamera)
     client.publish(topic, message)
-    logging.info(f"Published message to topic {topic}")
+    logger.info(f"Published message to topic {topic}")
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
